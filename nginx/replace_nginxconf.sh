81341e1929ccaa9a6879c5f7dceb4fe42bbf4838
dansta <dansta@fedora.localdomain>
2017-12-05 14:18:26 +0100

    Lets try this then

--- nginx/replace_nginxconf.sh
+++ nginx/replace_nginxconf.sh
@@ -4,6 +4,7 @@ set -e
 set -x
 CONF="/etc/nginx/nginx.conf"
 CONFDATA="$(cat $CONF)"
+SED_DATA=$CONFDATA
 KEY="awk -f /usr/local/bin/key.awk"
 VALUE="awk -f /usr/local/bin/value.awk"
 
@@ -12,13 +13,11 @@ NGINXENV="$(env | grep NGINX)"
 
 # for each nginxvar, replace them in the file
 for i in $NGINXENV; do
-  echo $i
-  EDITED_DATA="$( echo $CONFDATA | \
-               sed s/$(echo $i | $KEY)/$( echo $i | $VALUE )/g)"
+  SED_DATA="$( echo $SED_DATA | sed s/$(echo $i | $KEY )/$( echo $i | $VALUE )/g)"
 done
 
 # write it to file
-echo "$EDITED_DATA" > "$CONF"
+echo "$SED_DATA" > "$CONF"
 
 #clear out all comments and write to file
 #sed /^#.*$/d "$CONFDATA" > "$CONF"
