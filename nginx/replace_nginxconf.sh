b8e09cc7c396091e3c40ba1dca31d4c89dc0d070
dansta <dansta@fedora.localdomain>
2017-12-05 13:04:54 +0100

    We try this the

--- nginx/replace_nginxconf.sh
+++ nginx/replace_nginxconf.sh
@@ -12,7 +12,8 @@ NGINXENV="$(set | grep NGINX)"
 
 # for each nginxvar, replace them in the file
 for i in $NGINXENV; do
-  CONFDATA=$( "${CONFDATA}"//"$(echo "$i" | awk -f $EXECPATH/key.awk)"/"$(echo "$i" | awk -f $EXECPATH/value.awk)"/ ) 
+#CONFDATA=$( "${CONFDATA}"//"$(echo "$i" | awk -f $EXECPATH/key.awk)"/"$(echo "$i" | awk -f $EXECPATH/value.awk)"/ ) 
+  CONFDATA=$( sed s/"$(echo "$i" | awk -f $EXECPATH/key.awk)"/"$(echo "$i" | awk -f $EXECPATH/value.awk)"/g )
 done
 
 # write it to file
