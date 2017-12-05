41f28718ec415bf26933f961390716f9185cf69b
dansta <dansta@fedora.localdomain>
2017-12-05 12:35:28 +0100

    Data is a variable not a file

--- nginx/replace_nginxconf.sh
+++ nginx/replace_nginxconf.sh
@@ -12,7 +12,7 @@ NGINXENV="$(set | grep NGINX)"
 
 # for each nginxvar, replace them in the file
 for i in $NGINXENV; do
-  CONFDATA=$(sed s/"$(echo "$i" | awk -f $EXECPATH/key.awk)"/"$(echo "$i" | awk -f $EXECPATH/value.awk)"/g "$CONFDATA") 
+  CONFDATA=$(echo "$CONFDATA" | sed s/"$(echo "$i" | awk -f $EXECPATH/key.awk)"/"$(echo "$i" | awk -f $EXECPATH/value.awk)"/g) 
 done
 
 # write it to file
