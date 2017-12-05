7b6ca43b45ec91355f44a745f07de015a8c63e6d
dansta <dansta@fedora.localdomain>
2017-12-05 12:25:57 +0100

    Fix data replacement
    Read the config file once, put it in a data variable. Reiterate over that variable and finally write to file

--- nginx/replace_nginxconf.sh
+++ nginx/replace_nginxconf.sh
@@ -3,18 +3,20 @@
 set -e
 set -x
 CONF="/etc/nginx/nginx.conf"
+CONFDATA="$(cat $CONF)"
 EXECPATH="/usr/local/bin"
 
 # get all the nginxvars
 NGINXENV="$(set | grep NGINX)"
 
+
 # for each nginxvar, replace them in the file
 for i in $NGINXENV; do
-  CONFDATA=$(sed s/"$(echo "$i" | awk -f $EXECPATH/key.awk)"/"$(echo "$i" | awk -f $EXECPATH/value.awk)"/g "$CONF") 
+  CONFDATA=$(sed s/"$(echo "$i" | awk -f $EXECPATH/key.awk)"/"$(echo "$i" | awk -f $EXECPATH/value.awk)"/g "$CONFDATA") 
 done
 
 # write it to file
-echo $CONFDATA > $CONF
+echo "$CONFDATA" > "$CONF"
 
 #clear out all comments and write to file
 #sed /^#.*$/d "$CONFDATA" > "$CONF"
