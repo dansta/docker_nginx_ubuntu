20ba01c5f1901f0a53a0ee57d785ef784c03873e
dansta <dansta@fedora.localdomain>
2017-12-05 11:14:33 +0100

    This project gets its own repository

---
+++ nginx/replace_nginxconf.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# set up env
+set -e
+set -x
+CONF="/etc/nginx/nginx.conf"
+EXECPATH="/usr/local/bin"
+
+# get all the nginxvars
+NGINXENV="$(set | grep NGINX)"
+
+# for each nginxvar, replace them in the file
+for i in $NGINXENV; do
+  CONFDATA=$(sed s/"$(echo "$i" | awk -f $EXECPATH/key.awk)"/"$(echo "$i" | awk -f $EXECPATH/value.awk)"/g "$CONF") 
+done
+
+#clear out all comments and write to file
+sed /^#.*$/d "$CONFDATA" > "$CONF"
