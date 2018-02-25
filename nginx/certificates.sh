94cd148d7469fbe1fca38515caa9e665f645d52f
dansta <danne@kvantmekanik.se>
2018-02-25 20:38:10 +0100

    Adding support for LetsEncrypt and volume based content

--- nginx/certificates.sh
+++ nginx/certificates.sh
@@ -3,18 +3,19 @@
 # Using simp_le for now
 
 # Change this to your domain
-domain=example.com
-you=someone
+domain=$1
+you=$2
 
 docker run --rm \
     -v /etc/nginx/certificates:/simp_le/www \
     -v "$PWD":/certificates/certs \
     zenhack/simp_le \
-    --email $you@$domain \
+    --email "$you@$domain" \
     -f account_key.json \
     -f fullchain.pem \
-    -f $domain.pem \
-    -d $domain.com \
-    -d www.$domain \
-    -d mail.$domain \
+    -f cert.pem \
+    -f key.pem \
+    -d "$domain.com" \
+    -d "www.$domain" \
+    -d "mail.$domain" \
     --default_root /simp_le/www
--- nginx/makeservice.sh
+++ nginx/makeservice.sh
@@ -2,6 +2,8 @@
 
 # Run as root in the same directory as the Dockerfile
 label="nginx.0.0.1"
+domain=example.com
+you=me
 
 # Create image
 docker build -t $label .
@@ -17,29 +19,30 @@ docker network create \
 # Create volume for bind
 docker volume create nginx
 docker volume create certificates
+docker volume create htmlcontent
 
 # Create default html directory (replace contents with your own)
 mkdir -p /var/local/nginx/"$label"
 
-# Start the certificate service
-./certificates.sh
-
 # Create service
 docker service create \
-            --env NGINX_DOMAIN=example.com \
-            --env NGINX_WWWDOMAIN=www.example.com \
+            --env NGINX_DOMAIN=$domain \
+            --env NGINX_WWWDOMAIN=www.$domain \
             --mode global \
             --update-delay 60s \
             --update-parallelism 1 \
             --dns 127.0.0.1 \
             --network nginx \
             --log-driver syslog \
-            --mount source=nginx,target=/var/log/ \
+            --mount source=nginxlogs,target=/var/log/ \
+            --mount source=certificates,target=/usr/share/nginx/html/ \
+            --mount source=htmlcontent,target=/usr/share/nginx/html/ \
             --mount type=bind,source=/var/local/nginx/"$label",target=/var/local/nginx/,readonly \
-            --mount source=certificates,target=/etc/nginx/certificates/ \
             --name "$label" \
             --publish published=80,target=80,protocol=tcp \
             --publish published=443,target=443,protocol=tcp \
             $label
 
 
+# Start the certificate service
+./certificates.sh $domain $me
