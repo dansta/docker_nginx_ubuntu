6206b8672496e043b849f8e1946f87b3da83b3d9
dansta <danne@kvantmekanik.se>
2018-03-04 12:38:37 +0100

    Debugging some parts that do not build

--- nginx/makeservice.sh
+++ nginx/makeservice.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
+set -e
+set -x
+
 # You will want to change these parameters to your own
 label_cert="certbot"
 label_webserver="nginx:0.0.1"
+label_friendly="www_example_com"
 domain="example.com"
 you="me"
 
@@ -13,16 +17,16 @@ fi
 
 
 # Create images
-docker build -t $label_cert -f Dockerfile_certbot .
-docker build -t $label_webserver -f Dockerfile_webserver .
+docker build --build-arg=NGINX_DOMAIN="$domain" \
+             --build-arg=NGINX_WWWDOMAIN="www.$domain" \
+            -t "$label_webserver" \
+            -f Dockerfile_webserver \
+            .
 
-# Create east-west and multicast network
-docker network create \
-            --opt encrypted \
-            --subnet 10.0.0.0/24 \
-            --subnet 224.0.0.0/24 \
-            --driver overlay \
-            nginx 
+docker build -t $label_cert \
+            -f Dockerfile_certbot \
+            .
+
 
 # Create volume for bind
 docker volume create nginxlogs
@@ -30,9 +34,10 @@ docker volume create nginxconf
 docker volume create certificates
 
 # Create default html directory (replace contents with your own)
-mkdir -p /var/local/nginx/"$label_webserver"
+mkdir -p /var/local/nginx/"$label_friendly"
 
 # Create service
+if 
 docker service create \
             --env NGINX_DOMAIN=$domain \
             --env NGINX_WWWDOMAIN=www.$domain \
@@ -40,27 +45,28 @@ docker service create \
             --update-delay 60s \
             --update-parallelism 1 \
             --dns 127.0.0.1 \
-            --network nginx \
-            --log-driver syslog \
+            --name "$label_friendly" \
             --mount source=nginxconfig,target=/etc/nginx/,readonly \
             --mount source=nginxlogs,target=/var/log/ \
             --mount source=certificates,target=/usr/share/nginx/html/,readonly \
-            --mount type=bind,source=/var/local/nginx/"$label_webserver",target=/usr/share/nginx/html/,readonly \
-            --name "$label_webserver" \
             --publish published=80,target=80,protocol=tcp \
             --publish published=443,target=443,protocol=tcp \
             $label_webserver
-
+            then
 
 # Start the certificate service, disabled because currently we are making it inhouse
-docker run -d \
-           -e NGINX_DOMAIN=$domain \
-           -e NGINX_ADMIN=$you \
-           --name="$label_cert" \
-           --restart on-failure \
-           --mount source=certificates,target=/usr/share/nginx/html \
-           --mount source=nginxconf,target=/etc/nginx/ \
-           $label_cert
+        docker run -d \
+                   -e NGINX_DOMAIN=$domain \
+                   -e NGINX_ADMIN=$you \
+                   --name="$label_cert" \
+                   --restart on-failure \
+                   --mount source=certificates,target=/usr/share/nginx/html/ \
+                   --mount source=nginxconf,target=/etc/nginx/ \
+                   $label_cert
+            else
+            echo "Service creation failed"
+            exit 1
+fi
 
 
 echo
